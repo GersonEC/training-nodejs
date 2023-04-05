655864aca55f4cb0a0cc0a6b03b80023631849f8
GersonEC <gersonenriquez94@gmail.com>
2023-04-05 15:46:25 +0200

    Add consumer-http-basic.js file

---
+++ distribute-node/web-api/consumer-http-basic.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+// npm install fastify@3.2 node-fetch@2.6
+// Warning: Not as efficient as using a Reverse Proxy
+import fastify from 'fastify';
+import fetch from 'node-fetch';
+
+const server = fastify();
+
+const HOST = '127.0.0.1';
+const PORT = process.env.PORT || 3000;
+const TARGET = process.env.TARGET || 'localhost:4000';
+
+server.get('/', async () => {
+  const req = await fetch(`http://${TARGET}/recipes/42`);
+  const payload = await req.json();
+
+  return {
+    consumer_pid: process.pid,
+    producer_data: payload,
+  };
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Consumer running at http://${HOST}:${PORT}/`);
+});
